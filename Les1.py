c4eb5e0aa664ef7b990f51a8c3ffa23ed25bd306
Alesik-k <zeronemka@gmail.com>
2022-09-17 21:26:49 +0300

    Типы данных Python

--- Les1.py
+++ Les1.py
@@ -1,1 +1,19 @@
-Hello
+per1_1 = 10  # int
+print(per1_1)
+per1_2 = 2
+print(per1_2)
+per2_1 = 1.35 #float
+print(per2_1)
+per2_2 = 2.45
+print(per2_2)
+per3_1 = True #bool
+print(per3_1)
+per3_2 = False
+print(per3_2)
+per4_1 = "Hello" #str
+print(per4_1)
+per4_2 = "How are you?"
+print(per4_2)
+per5_1 = None #none
+print(per5_1)
+
