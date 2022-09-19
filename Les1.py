e054881f92874126fafb382eab98e84f36e26b75
Alesik-k <zeronemka@gmail.com>
2022-09-20 00:01:47 +0300

    Python data types - task 1

--- Les1.py
+++ Les1.py
@@ -1,6 +1,12 @@
-str1="hello"
-str2="123"
-print(type(str1))
-print(type(str2))
-number2=int(str2)
-print(type(number2))
+print(1**100)
+print(2**16)
+print(2-1000)
+print(2+2*2)
+print(60/3)
+import math
+print(math.sqrt(144))
+x=2+5*14
+x=66-11
+print(x)
+x=(16-8)**2
+print(x)
