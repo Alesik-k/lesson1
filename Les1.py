d9225fd26412c3007eaef659dc0097ff8ef8cf5d
Alesik-k <zeronemka@gmail.com>
2022-09-26 12:51:05 +0300

    Задача про треугольник и "Мне n лет"

--- Les1.py
+++ Les1.py
@@ -4,15 +4,20 @@ c=7
 triangle=(a+b)>c #Сумма длин двух любых сторон треугольника больше длины оставшейся стороны
 print(triangle)
 
-n=2
-age=str(n)
-if n>=11 and n<=19:
-    print("Мне",n,"лет")
-elif int(age[-1::])==0:
-    print("Мне",n,"лет")
-elif int(age[-1::])>=5 and int(age[-1::])<=9:
-    print("Мне",n,"лет")
-elif int(age[-1::])==1:
+n=99
+y=str(n)
+if int(y[-1::])==1 and y!="11":
     print("Мне",n,"год")
+elif int(y[-1::])>=2 and int(y[-1::])<=4 and y!=range(12,14):
+    print("Мне",n,"годa")
 else:
-    print("Мне", n, "года")
+    print("Мне",n, "лет")
+
+n1=21
+a=n1 % 10
+if a==1 and a!="11":
+    print("Мне",n1,"год")
+elif a>=2 and a<=4 and a!=range(12,14):
+    print("Мне",n1,"годa")
+else:
+    print("Мне",n1,"лет")
