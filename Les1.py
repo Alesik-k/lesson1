821eb2d1db994d01ac81e3c27f96fde3ad3b3295
Alesik-k <zeronemka@gmail.com>
2022-10-02 23:34:08 +0300

    Найти сумму и произведение цифр введённого трёхзначного натурального числа.

--- Les1.py
+++ Les1.py
@@ -1,17 +1,9 @@
-try:
-    year1=int(input("BD year 1 : "))
-    year2=int(input("BD year 2 : "))
-    month1=int(input("BD month 1 : "))
-    month2=int(input("BD month 2 : "))
-    day1=int(input("BD day 1 : "))
-    day2=int(input("BD day 2 : "))
-    if year1<year2:
-        print("Person 1 is older")
-    elif year1==year2 and month1<month2:
-        print("Person 1 is older")
-    elif year1 == year2 and month1==month2 and day1<day2:
-        print("Person 1 is older")
-    else:
-        print("Person 2 is older")
-except ValueError:
-    print("Input only numbers!")
+a=int(input("Enter a three-digit number: "))
+if 99<a<1000:
+    y=str(a)
+    print(int(y[:1:1])+int(y[1:2:1])+int(y[2:3:1]))
+    print(int(y[:1:1])*int(y[1:2:1])*int(y[2:3:1]))
+else:
+    print("The number isn't correct")
+
+
