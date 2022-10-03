c88172a2396256ceff532d486c80f9e57a47e74e
Alesik-k <zeronemka@gmail.com>
2022-10-03 23:55:55 +0300

    Таблица умножения

--- Les1.py
+++ Les1.py
@@ -1,9 +1,4 @@
-a=int(input("Enter a three-digit number: "))
-if 99<a<1000:
-    y=str(a)
-    print(int(y[:1:1])+int(y[1:2:1])+int(y[2:3:1]))
-    print(int(y[:1:1])*int(y[1:2:1])*int(y[2:3:1]))
-else:
-    print("The number isn't correct")
-
-
+for i in range(1,10):
+    for j in range(1,11):
+        print(i*j,end="\t")
+    print("\n")
