1f2a5f07097a37f6d2f3dda5d8ca2f6011586b81
Alesik-k <zeronemka@gmail.com>
2022-10-12 23:45:28 +0300

    Фибоначчи и факториал

--- Les1.py
+++ Les1.py
@@ -1,11 +1,20 @@
-#Найдите его наибольшую цифру в числе
-n=input("Enter your number 1: ")
-a=0
-for i in n:
-    if int(i)>int(a):
-        a=i
-print(a)
+# Вывести на экран столько элементов ряда Фибоначчи (рядом чисел, в котором каждое
+# последующее число является суммой двух предыдущих), сколько указал пользователь.
+n=int(input("Enter your number fobonachchi: "))
+f_n_2=f_n_1=1
+print(f_n_1,f_n_2, end=" ")
+for i in range(2,n):
+    fn=f_n_2+f_n_1
+    f_n_2=f_n_1
+    f_n_1=fn
+    print(fn, end=" ")
 
-#Сформируйте из введённого числа обратное по порядку входящих в него цифр и выведите на экран.
-y=input("Enter your number 2: ")
-print(y[::-1])
+print()
+#factorial
+a=int(input("Enter your number for factorial: "))
+i=1
+factorial=1
+while i<=a:
+     factorial*=i
+     i+=1
+print("Facrotial for", a, "is", factorial)
