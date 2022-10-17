3b574e82532f8c39b5459956fa639c8a8d5c2fb9
Alesik-k <zeronemka@gmail.com>
2022-10-17 23:51:38 +0300

    Совершенные числа

--- Les1.py
+++ Les1.py
@@ -1,20 +1,9 @@
-# Вывести на экран столько элементов ряда Фибоначчи (рядом чисел, в котором каждое
-# последующее число является суммой двух предыдущих), сколько указал пользователь.
-n=int(input("Enter your number fobonachchi: "))
-f_n_2=f_n_1=1
-print(f_n_1,f_n_2, end=" ")
-for i in range(2,n):
-    fn=f_n_2+f_n_1
-    f_n_2=f_n_1
-    f_n_1=fn
-    print(fn, end=" ")
-
-print()
-#factorial
-a=int(input("Enter your number for factorial: "))
-i=1
-factorial=1
-while i<=a:
-     factorial*=i
-     i+=1
-print("Facrotial for", a, "is", factorial)
+# Совершенные числа - найти все совершенные числа до chisla1
+chislo1=int(input("Enter your number: "))
+for i in range(1,chislo1+1):
+    summ=0
+    for j in range(1,i):
+        if i%j==0:
+            summ+=j
+    if i==summ:
+        print((str(i)+ " совершенное число"))
